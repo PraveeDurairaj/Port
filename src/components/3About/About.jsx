67aeae90358531f3c1f2806aeae29169843ed93c
PraveeDurairaj <duraipraveen8834@gmail.com>
2024-02-13 20:04:09 -0800

    change photo

--- src/components/3About/About.jsx
+++ src/components/3About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Col, Container, Row,Image } from 'react-bootstrap'
-import photo from './praveen.jpg'
+import photo from './myPhoto.png'
 import './About.css'
 
 const About = () => {
@@ -9,7 +9,7 @@ const About = () => {
             <h1 className='d-flex justify-content-center pt-2 '>About <span className='ps-2' style={{'color':'yellow'}}>Me</span></h1>
             <Row>
                 <Col md={4} className='mt-3 d-flex justify-content-center'>
-                    <Image src={photo} fluid/>
+                    <Image src={photo} fluid className='img'/>
                 </Col>
                 <Col className='mt-5 d-flex align-items-center px-2 text-justify'>
                     <p id='about-container-para'>I am Praveen, a passionate React developer with expertise in HTML, CSS, and JavaScript. 
